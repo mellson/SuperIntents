229b12da5547f5c18060eab88df66fa3ba0aa927
Anders Bech Mellson <anders@bechmellson.com>
2013-04-16 15:56:46 +0200

    Added tuplehelper and updated SIHelper

---
+++ src/superintents/control/ASTTupleHelper.java
@@ -0,0 +1,21 @@
+package superintents.control;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class ASTTupleHelper {
+	public ASTRewrite rewriter;
+	public ITextEditor editor;
+	ICompilationUnit unit;
+	MethodDeclaration currentMethod;
+	
+	public ASTTupleHelper(ASTRewrite rewriter, ITextEditor editor, ICompilationUnit unit, MethodDeclaration currentMethod) {
+		super();
+		this.rewriter = rewriter;
+		this.editor = editor;
+		this.unit = unit;
+		this.currentMethod = currentMethod;
+	}
+}
--- src/superintents/control/SIHelper.java
+++ src/superintents/control/SIHelper.java
@@ -17,70 +17,81 @@ import transformers.Java2AST;
 public class SIHelper {
 	public static void insertIntent(SuperIntentImpl intentImplementaion) {
 		ArrayList<ASTNodeWrapper> nodes = Java2AST.transformSuperIntent(intentImplementaion);
-		for (ASTNodeWrapper node : nodes) {
-			try {
-				if (node.placeInsideMethod) {
-					insertNodeIntoCurrentMethod(node);
-				} else {
-
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		try {
+			insertNodeIntoCurrentMethod(nodes);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static void insertNodeIntoCurrentMethod(ASTNodeWrapper node) throws MalformedTreeException, BadLocationException, JavaModelException {
+	private static ASTTupleHelper getASTTupleHelper() {
+		ASTRewrite rewriter = null;
+		ICompilationUnit unit = null;
 		ITextEditor editor = getEditor();
+		MethodDeclaration currentMethod = null;
 		IEditorInput editorInput = editor.getEditorInput();
 		IJavaElement elem = JavaUI.getEditorInputJavaElement(editorInput);
 		if (elem instanceof ICompilationUnit) {
-			ICompilationUnit unit = (ICompilationUnit) elem;
+			unit = (ICompilationUnit) elem;
 			CompilationUnit astRoot = parse(unit);
 			AST ast = astRoot.getAST();
-			ASTRewrite rewriter = ASTRewrite.create(ast);
+			rewriter = ASTRewrite.create(ast);
 			TypeDeclaration typeDecl = (TypeDeclaration) astRoot.types().get(0);
-			int offset = getCurrentMethodOffset(typeDecl.getMethods());
-			MethodDeclaration methodDecl = typeDecl.getMethods()[offset];
-			Block block = methodDecl.getBody();
+			int currentMethodOffset = getCurrentMethodOffset(typeDecl.getMethods());
+			currentMethod = typeDecl.getMethods()[currentMethodOffset];
 
-			// Where in the statement list of the method should the node be added?
+		}
+		return new ASTTupleHelper(rewriter, editor, unit, currentMethod);
+	}
+
+	private static void insertNodeIntoCurrentMethod(ArrayList<ASTNodeWrapper> nodes) throws MalformedTreeException, BadLocationException, JavaModelException {
+		ASTTupleHelper helper = getASTTupleHelper();
+		Block block = helper.currentMethod.getBody();
+
+		// Where in the statement list of the method should the node be added?
+		int blockOffset = block.getStartPosition();
+		int caretOffset = getCaretOffset(helper.editor);
+
+		// Running through the nodes in reversed order
+		for (int i = nodes.size() - 1; i >= 0; i--) {
+			ASTNodeWrapper node = nodes.get(i);
 			int nodeStatementOffset = 0;
-			int nodeLength = 0;
-			int blockOffset = block.getStartPosition();
-			int caretOffset = getCaretOffset(editor);
-
-			/*
-			 * Run through all statements and compare their position to the carets position. If the statement is before the caret, increase the nodes insert point by 1.
-			 */
+			// Run through all statements and compare their position to the carets position
+			// If the statement is before the caret, increase the nodes insert point by 1 which leads to inserting after that statement
 			int statementOffset = blockOffset;
+			Block nestedStatement = null;
+			
 			for (Object o : block.statements()) {
 				statementOffset += o.toString().length();
 				nodeStatementOffset += (statementOffset < caretOffset) ? 1 : 0;
+				nestedStatement = (Block) isCaretInStatement((Statement) o, caretOffset);
 			}
-
-			ListRewrite listRewrite = rewriter.getListRewrite(block, Block.STATEMENTS_PROPERTY);
+			
+			ListRewrite listRewrite = null;
+			if (nestedStatement != null) {
+				nodeStatementOffset = 0; // FIX THIS!!!
+				listRewrite = helper.rewriter.getListRewrite(nestedStatement, Block.STATEMENTS_PROPERTY);
+			} else
+				listRewrite = helper.rewriter.getListRewrite(block, Block.STATEMENTS_PROPERTY);
+			
 			if (node.isComment) {
-				ASTNode commentNode = rewriter.createStringPlaceholder(node.comment, ASTNode.EMPTY_STATEMENT);
+				ASTNode commentNode = helper.rewriter.createStringPlaceholder(node.comment, ASTNode.EMPTY_STATEMENT);
 				listRewrite.insertAt(commentNode, nodeStatementOffset, null);
-				nodeLength += node.comment.length() + 3;
 			} else {
 				listRewrite.insertAt(node.node, nodeStatementOffset, null);
 				nodeStatementOffset += 1;
-				nodeLength += node.node.toString().length() + 2;
 			}
-
-			// Delete the current line when inserting!!!
-			TextEdit edits = rewriter.rewriteAST();
-			Document document = new Document(unit.getSource());
-			edits.apply(document);
-			unit.getBuffer().setContents(document.get());
-
-			// Jump to the position right after the inserted node and focus the editor
-			caretOffset += nodeLength;
-			editor.selectAndReveal(caretOffset, 0);
-			editor.setFocus();
+			
 		}
+
+		TextEdit edits = helper.rewriter.rewriteAST();
+		Document document = new Document(helper.unit.getSource());
+		edits.apply(document);
+		helper.unit.getBuffer().setContents(document.get());
+		
+		// Jump to the position right after the inserted node and focus the editor
+		helper.editor.selectAndReveal(edits.getExclusiveEnd(), 0);
+		helper.editor.setFocus();
 	}
 
 	protected static CompilationUnit parse(ICompilationUnit unit) {
@@ -115,5 +126,45 @@ public class SIHelper {
 				result = i;
 		}
 		return result;
+	}
+
+	/**
+	 * This method will check whether the caret is placed inside the given statement and return the innermost ASTNode to insert new nodes in.
+	 * @param o A statement
+	 * @param caretOffset the offset of the caret
+	 * @return the ASTnode to insert new nodes in
+	 */
+	protected static ASTNode isCaretInStatement(Statement o, int caretOffset) {
+		ASTNode resultNode = null;
+
+		// find where the statements starts and stops
+		int statementStartPos = ((Statement) o).getStartPosition();
+		int statementEndPos = ((Statement) o).getStartPosition() + ((Statement) o).getLength();
+
+		// if the caret is placed in a statement...
+		if (caretOffset > statementStartPos && caretOffset < statementEndPos) {
+			if (o.getClass().equals(Block.class)) {
+				for (Object s : ((Block) o).statements()) {
+					return isCaretInStatement((Statement) s, caretOffset);
+				}
+			}
+
+			ChildPropertyDescriptor cpd = null;
+
+			// ...we check that statements structural properties....
+			for (Object s : o.structuralPropertiesForType()) {
+				if (s.getClass() == ChildPropertyDescriptor.class) {
+					// ...to find out if it has a "BODY" property (ex. ForStatements have these)
+					if (((ChildPropertyDescriptor) s).getId().equals("body"))
+						cpd = (ChildPropertyDescriptor) s;
+				}
+			}
+			
+			if (cpd != null)
+				resultNode = (ASTNode) isCaretInStatement((Statement) o.getStructuralProperty(cpd), caretOffset);
+			else
+				resultNode = o;
+		}
+		return resultNode;
 	}
 }
