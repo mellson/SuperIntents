b5c36d19506ecbdcab20346c9f5c8c68cbcdd6c2
Anders Bech Mellson <anders@bechmellson.com>
2013-04-20 16:06:27 +0200

    added java identifier method

--- src/superintents/util/Java2AST.java
+++ src/superintents/util/Java2AST.java
@@ -476,5 +476,23 @@ public class Java2AST {
 		
 		return astv.getExists();
 	}
+	
+	public final static boolean isValidJavaIdentifier(String s) {
+	 // an empty or null string cannot be a valid identifier
+	 if (s == null || s.length() == 0)
+	    return false;
+
+	 char[] c = s.toCharArray();
+	 if (!Character.isJavaIdentifierStart(c[0])) {
+	    return false;
+	 }
+
+	 for (int i = 1; i < c.length; i++) {
+	    if (!Character.isJavaIdentifierPart(c[i]))
+	       return false;
+	 }
+
+	 return true;
+	}
 }
 	
