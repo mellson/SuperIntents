296251b5d740673ccb183baf22d6cfe0311ce72b
Anders Bech Mellson <anders@bechmellson.com>
2013-04-24 20:26:22 +0200

    !!! This update will break functionality !!!
    Updated to support being installed as plugin.
    You have to run an external eclipse with the plugin for it to work.

--- src/intentloader/IntentLoader.java
+++ src/intentloader/IntentLoader.java
@@ -4,56 +4,117 @@ import java.net.URL;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Map;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.util.*;
-
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import intentmodel.IntentmodelPackage;
 import intentmodel.impl.SuperIntentImpl;
 
 public class IntentLoader {
-
-	
-		public ArrayList<String> loadListOfIntentFiles(){
+	public static ArrayList<String> loadListOfIntentFilesFromJar() {
+		// Initialize the model
+		IntentmodelPackage.eINSTANCE.eClass();
+		ArrayList<String> output = new ArrayList<String>();
+		try {
+			// Locate the jar file of this plug-in
 			CodeSource src = IntentLoader.class.getProtectionDomain().getCodeSource();
-			URL jar = src.getLocation();
-			String path = jar.toString();
-			path = path.replace("file:", "");
-			path = path + "src/resources";
-			File f = new File(path);
+			if (src != null) {
+				URL jar = src.getLocation();
+				ZipInputStream zip = new ZipInputStream(jar.openStream());
+				ZipEntry ze = null;
+				while ((ze = zip.getNextEntry()) != null) {
+					if (ze.getName().startsWith("resources/") && ze.getName().endsWith(".intentmodel")) {
+						output.add(ze.getName().replace("resources/", "").replace(".intentmodel", ""));
+					}
+				}
+
+			}
+		} catch (Exception e) {
+		}
+		return output;
+	}
+
+	public static SuperIntentImpl loadIntentInstanceFromJar(String filename) {
+		// Initialize the model
+		IntentmodelPackage.eINSTANCE.eClass();
+		SuperIntentImpl ss = null;
+		try {
+			// Locate the jar file of this plug-in
+			CodeSource src = IntentLoader.class.getProtectionDomain().getCodeSource();
+			if (src != null) {
+				URL jar = src.getLocation();
+				ZipInputStream zip = new ZipInputStream(jar.openStream());
+				ZipEntry ze = null;
+				while ((ze = zip.getNextEntry()) != null) {
+//					if (ze.getName().equals("resources/"+filename+".intentmodel")) {
+						if (ze.getName().contains(filename) && ze.getName().endsWith(".intentmodel")) {
+						// Copy the input stream
+						byte[] buffer = new byte[(int)ze.getSize()];
+					    while (zip.available() > 0) {
+					    	zip.read(buffer, 0, buffer.length);
+					    }
+					    InputStream is = new ByteArrayInputStream(buffer);
+						
+					    // Create a new SuperIntentImpl from the input stream 
+						Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+						Map<String, Object> m = reg.getExtensionToFactoryMap();
+						m.put("intentmodel", new XMIResourceFactoryImpl());
+						XMIResourceImpl xi = new XMIResourceImpl();
+						xi.load(is, m);
+						ss = (SuperIntentImpl) xi.getContents().get(0);
+						
+						zip.close();
+						return ss;
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ss;
+	}
+	
+	// These two methods below are used when running the plugin from inside Eclipse - when installed as a plug-in use the two methods above
+	public static ArrayList<String> loadListOfIntentFilesFromEclipse() {
+		URL url;
+		ArrayList<String> output = new ArrayList<String>();
+		try {
+			url = new URL("platform:/plugin/SuperIntents/src/resources");
+			File f = new File(FileLocator.resolve(url).toURI());
 			ArrayList<String> names = new ArrayList<String>(Arrays.asList(f.list()));
-			ArrayList<String> output = new ArrayList<String>();
-			
-			for(String e : names){
+			for (String e : names) {
 				output.add(e.replace(".intentmodel", ""));
 			}
-			
-			return output;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		public SuperIntentImpl loadIntentInstance(String filename) {
-			 // Initialize the model
-		    IntentmodelPackage.eINSTANCE.eClass();
-		    
-		    // Register the XMI resource factory for the .intentmodel extension
-		    Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-		    Map<String, Object> m = reg.getExtensionToFactoryMap();
-		    m.put("intentmodel", new XMIResourceFactoryImpl());
+		return output;
+	}
 
-		    // Obtain a new resource set
-		    ResourceSet resSet = new ResourceSetImpl();
-		    
-		    // Get the resource
-		    Resource resource = resSet.getResource(URI.createURI((IntentLoader.class.getResource("/resources/"+filename+".intentmodel")).toString()), true);
-		    
-		    //Cast resource to SuperIntent
-		    SuperIntentImpl superintent = (SuperIntentImpl) resource.getContents().get(0);
-		    
-		    return superintent;
-		}
+	public static SuperIntentImpl loadIntentInstanceFromEclipse(String filename) {
+		// Initialize the model
+		IntentmodelPackage.eINSTANCE.eClass();
+
+		// Obtain a new resource set
+		ResourceSet resSet = new ResourceSetImpl();
+		
+		// Get the resource
+		Resource resource = resSet.getResource(URI.createURI((IntentLoader.class.getResource("/resources/" + filename + ".intentmodel")).toString()),true);
+		
+		// Cast resource to SuperIntent
+		SuperIntentImpl superintent = (SuperIntentImpl) resource.getContents().get(0);
+		
+		return superintent;
+	}
 }
--- src/superintents/views/SuperIntentsView.java
+++ src/superintents/views/SuperIntentsView.java
@@ -1,6 +1,5 @@
 package superintents.views;
 
-
 import intentloader.IntentLoader;
 
 import java.util.ArrayList;
@@ -33,22 +32,12 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 import superintents.util.JDTInserter;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view shows data obtained from the model. The sample creates a dummy model on the fly, but a real implementation would connect
+ * to the model available either in this or another plug-in (e.g. the workspace). The view is connected to the model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be presented in the view. Each view can present the same model objects using different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views in order to ensure that objects of the same type are presented in the same way everywhere.
  * <p>
  */
 
@@ -64,39 +53,71 @@ public class SuperIntentsView extends ViewPart {
 	private Action doubleClickAction;
 
 	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
+	 * The content provider class is responsible for providing objects to the view. It can wrap existing objects in adapters or simply return objects as-is. These objects may be sensitive to the
+	 * current input of the view, or ignore it and always show the same content (like Task List, for example).
 	 */
-	 
+
 	class ViewContentProvider implements IStructuredContentProvider {
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
 		}
-		
+
 		public void dispose() {
 		}
-		
+
 		public Object[] getElements(Object parent) {
-			ArrayList<String> lst = (new IntentLoader()).loadListOfIntentFiles();
+			// Load intents from Jar or Eclipse
+			
+			// TODO - Switch the two methods below in order for it to work when running inside Eclipse
+			ArrayList<String> lst = IntentLoader.loadListOfIntentFilesFromJar();
+			// ArrayList<String> lst = IntentLoader.loadListOfIntentFilesFromEclipse();
 			return lst.toArray();
 		}
 	}
+
+	private void makeActions() {
+		action1 = new Action() {
+			public void run() {
+				showMessage("Action 1 executed");
+			}
+		};
+		action1.setText("Action 1");
+		action1.setToolTipText("Action 1 tooltip");
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+
+		action2 = new Action() {
+			public void run() {
+				showMessage("Action 2 executed");
+			}
+		};
+		action2.setText("Action 2");
+		action2.setToolTipText("Action 2 tooltip");
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		doubleClickAction = new Action() {
+			public void run() {
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection) selection).getFirstElement();
+				
+				// TODO - Switch the two methods below in order for it to work when running inside Eclipse
+				JDTInserter.insertIntent(IntentLoader.loadIntentInstanceFromJar(obj.toString()));
+				// JDTInserter.insertIntent(IntentLoader.loadIntentInstanceFromEclipse(obj.toString()));
+			}
+		};
+	}
+
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
 			return getText(obj);
 		}
+
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
 		}
+
 		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_TOOL_PASTE);
+			return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_TOOL_PASTE);
 		}
 	}
+
 	class NameSorter extends ViewerSorter {
 	}
 
@@ -107,8 +128,7 @@ public class SuperIntentsView extends ViewPart {
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * This is a callback that will allow us to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
 		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
@@ -156,39 +176,10 @@ public class SuperIntentsView extends ViewPart {
 		// Other plug-ins can contribute there actions here
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(action1);
 		manager.add(action2);
-	}
-
-	private void makeActions() {
-		action1 = new Action() {
-			public void run() {
-				showMessage("Action 1 executed");
-			}
-		};
-		action1.setText("Action 1");
-		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
-		action2 = new Action() {
-			public void run() {
-				showMessage("Action 2 executed");
-			}
-		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		doubleClickAction = new Action() {
-			public void run() {
-				ISelection selection = viewer.getSelection();
-				Object obj = ((IStructuredSelection)selection).getFirstElement();
-				JDTInserter.insertIntent((new IntentLoader()).loadIntentInstance(obj.toString()));
-			}
-		};
 	}
 
 	private void hookDoubleClickAction() {
@@ -198,11 +189,9 @@ public class SuperIntentsView extends ViewPart {
 			}
 		});
 	}
+
 	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			viewer.getControl().getShell(),
-			"SuperIntents View",
-			message);
+		MessageDialog.openInformation(viewer.getControl().getShell(), "SuperIntents View", message);
 	}
 
 	/**
